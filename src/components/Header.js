459eda0087443a937be1fa360c5b6c7076c7c0f6
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-07-12 16:12:43 -0400

    finished hero section and added HTML markup on about section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import Logo from "../assets/logo.png";
+import avaterOne from "../assets/avater.png";
+import avaterTwo from "../assets/avaterTwo.png";
+import avaterThree from "../assets/avaterThree.png";
+import triangle from "../assets/triangle.png"
 
 
 const Header = () => {
     return (
-        <header>
+        <header className="gradientBackground">
             <nav className="navigation">
                 <div className="logo">
                     <img src={Logo} alt="Netbook Logo"/>
                     <h2>Netbook</h2>
                 </div>
                 <div className="menu">
-                    <ul><a href="#">Home</a></ul>
-                    <ul className="options"><a href="#">Community&#8964;</a></ul>
-                    <ul><a href="#">Blog</a></ul>
-                    <ul><a href="#">Events</a></ul>
+                    <ul><a href="/home">Home</a></ul>
+                    <ul className="options"><a href="/community">Community&#8964;</a></ul>
+                    <ul><a href="/blog">Blog</a></ul>
+                    <ul><a href="/events">Events</a></ul>
                 </div>
                 <div className="auth">
                     <form action="/search" method="GET">
@@ -24,23 +28,22 @@ const Header = () => {
             </nav>
             <div className="heroSection">
                 <div className="heroText">
-                    <h4>Netbook community</h4>
+                    <button className="subtitle">Netbook community</button>
                     <h1>your solutions for community!</h1>
-                    <p> More than 2 billion people in over countries use socibook to stay in touch with friends & family.</p>
+                    <p className="bodyText"> More than 2 billion people in over countries use socibook to stay in touch with friends & family.</p>
                     <div className="heroButtons">
-                        <button>About More</button>
-                        <button> Invite Friend</button>
+                        <button className="generalButton">About More</button>
+                        <button className="generalButton"> Invite Friend</button>
                     </div>
                 </div>
                 <div className="heroImage">
-                    <img src="#" alt="" />
-                    <img src="#" alt="" />
-                    <img src="#" alt="" />
+                    <img src={avaterOne} className="avaterOne" alt="character model of a woman looking right in a blue shirt and scarf" />
+                    <img src={avaterThree} height= {220} weight= {220}className="avaterThree" alt="character model of a man looking left in a yellow jacket" />
+                    <img className="avaterTwo" src={avaterTwo} alt="character model of a man looking left in a blue sweatshirt" />
+                    <img src={triangle} height= {400} weight= {400} className="triangle" alt="background dashed triangle connecting pictures" />
                 </div>
             </div>
-            
         </header>
     )
 }
-
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,31 @@
+import imageLeft from "../assets/imageLeft.png";
+import imageTop from "../assets/imageTop.png";
+import imageBottom from "../assets/imageBottom.png";
+
+
 const Main = () => {
     return (
-        <main>
-            <p>I forgot how to do this</p>
-        </main>
+        <div className="aboutUs">
+            <div className="aboutText">
+                <h5>What's Netbooks</h5>
+                <h3>Why Join Netbook's Social Network?</h3>
+                <p className="bodyText"> Recent surveys have indicated that small businesses recognize the need they have to connect with the consumer.</p>
+                <div className="aboutList">
+                    <ul>
+                        <li>Groups</li>
+                        <li>Messages</li>
+                        <li>Share</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="aboutImage">
+                <img src={imageLeft} className="imageLeft" alt="Group of people working on a table that has multiple laptops and tablets on it" />
+                <div className="imagePosition">
+                    <img src={imageTop} className="imageTop" alt="male sitting with cup of tea and laptop on table" />
+                    <img className="imageBottom" src={imageBottom} alt="bundle of ethernet cords connect the the base of module" />
+                </div>
+            </div>
+        </div>
     )
 }
 
