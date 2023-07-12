e855388c980af70cf0048bf97dbff86b17c6bac7
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-07-12 18:19:03 -0400

    Finished about section and completed HTML markup for achievements section

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,39 +8,41 @@ import triangle from "../assets/triangle.png"
 const Header = () => {
     return (
         <header className="gradientBackground">
-            <nav className="navigation">
-                <div className="logo">
-                    <img src={Logo} alt="Netbook Logo"/>
-                    <h2>Netbook</h2>
-                </div>
-                <div className="menu">
-                    <ul><a href="/home">Home</a></ul>
-                    <ul className="options"><a href="/community">Community&#8964;</a></ul>
-                    <ul><a href="/blog">Blog</a></ul>
-                    <ul><a href="/events">Events</a></ul>
-                </div>
-                <div className="auth">
-                    <form action="/search" method="GET">
-                    <input type="text" name="query" placeholder=" &#128269; Search Here..."/>
-                    </form>
-                    <button className="loginButton"> Log In </button>
-                </div>
-            </nav>
-            <div className="heroSection">
-                <div className="heroText">
-                    <button className="subtitle">Netbook community</button>
-                    <h1>your solutions for community!</h1>
-                    <p className="bodyText"> More than 2 billion people in over countries use socibook to stay in touch with friends & family.</p>
-                    <div className="heroButtons">
-                        <button className="generalButton">About More</button>
-                        <button className="generalButton"> Invite Friend</button>
+            <div className="wrapper">
+                <nav className="navigation">
+                    <div className="logo">
+                        <img src={Logo} alt="Netbook Logo" />
+                        <h2>Netbook</h2>
                     </div>
-                </div>
-                <div className="heroImage">
-                    <img src={avaterOne} className="avaterOne" alt="character model of a woman looking right in a blue shirt and scarf" />
-                    <img src={avaterThree} height= {220} weight= {220}className="avaterThree" alt="character model of a man looking left in a yellow jacket" />
-                    <img className="avaterTwo" src={avaterTwo} alt="character model of a man looking left in a blue sweatshirt" />
-                    <img src={triangle} height= {400} weight= {400} className="triangle" alt="background dashed triangle connecting pictures" />
+                    <div className="menu">
+                        <ul><a href="/home">Home</a></ul>
+                        <ul className="options"><a href="/community">Community&#8964;</a></ul>
+                        <ul><a href="/blog">Blog</a></ul>
+                        <ul><a href="/events">Events</a></ul>
+                    </div>
+                    <div className="auth">
+                        <form action="/search" method="GET">
+                            <input type="text" name="query" placeholder=" &#128269; Search Here..." />
+                        </form>
+                        <button className="loginButton"> Log In </button>
+                    </div>
+                </nav>
+                <div className="heroSection">
+                    <div className="heroText">
+                        <button className="subtitle">Netbook community</button>
+                        <h1>your solutions for community!</h1>
+                        <p className="bodyText"> More than 2 billion people in over countries use socibook to stay in touch with friends & family.</p>
+                        <div className="heroButtons">
+                            <button className="generalButton">About More</button>
+                            <button className="generalButton"> Invite Friend</button>
+                        </div>
+                    </div>
+                    <div className="heroImage">
+                        <img src={avaterOne} className="avaterOne" alt="character model of a woman looking right in a blue shirt and scarf" />
+                        <img src={avaterThree} height={220} weight={220} className="avaterThree" alt="character model of a man looking left in a yellow jacket" />
+                        <img className="avaterTwo" src={avaterTwo} alt="character model of a man looking left in a blue sweatshirt" />
+                        <img src={triangle} height={400} weight={400} className="triangle" alt="background dashed triangle connecting pictures" />
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,28 +5,87 @@ import imageBottom from "../assets/imageBottom.png";
 
 const Main = () => {
     return (
-        <div className="aboutUs">
-            <div className="aboutText">
-                <h5>What's Netbooks</h5>
-                <h3>Why Join Netbook's Social Network?</h3>
-                <p className="bodyText"> Recent surveys have indicated that small businesses recognize the need they have to connect with the consumer.</p>
-                <div className="aboutList">
-                    <ul>
-                        <li>Groups</li>
-                        <li>Messages</li>
-                        <li>Share</li>
-                    </ul>
+        <div className="wrapper">
+            <div className="aboutUs">
+                <div className="aboutText">
+                    <h5>What's Netbook?</h5>
+                    <h3>Why Join Netbook's Social Network?</h3>
+                    <p className="aboutDescription"> Recent surveys have indicated that small businesses recognize the need they have to connect with the consumer.</p>
+                    <div className="aboutList">
+                        <ul>
+                            <li>Groups</li>
+                            <li>Messages</li>
+                            <li>Share</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="aboutImage">
+                    <img src={imageLeft} className="imageLeft" width={310} height={400} alt="Group of people working on a table that has multiple laptops and tablets on it" />
+                    <div className="imagePosition">
+                        <img src={imageTop} className="imageTop" alt="male sitting with cup of tea and laptop on table" />
+                        <img className="imageBottom" src={imageBottom} alt="bundle of ethernet cords connect the the base of module" />
+                    </div>
                 </div>
             </div>
-            <div className="aboutImage">
-                <img src={imageLeft} className="imageLeft" alt="Group of people working on a table that has multiple laptops and tablets on it" />
-                <div className="imagePosition">
-                    <img src={imageTop} className="imageTop" alt="male sitting with cup of tea and laptop on table" />
-                    <img className="imageBottom" src={imageBottom} alt="bundle of ethernet cords connect the the base of module" />
+            <div className="acheivements">
+                <div className="ratings">
+                    <div className="cardTitle">
+                        <img src="#" alt="star icon" />
+                        <h4>4.8 Rating</h4>
+                    </div>
+                    <div className="membersContainer">
+                        <div className="membersPhoto">
+                            <img src="#" alt="small profile of Allen" />
+                            <img src="#" alt="small profile of Samantha" />
+                            <img src="#" alt="small profile of Jared" />
+                        </div>
+                        <div className="members">
+                            <p><span className="color">+836k </span>Members</p>
+                        </div>
+                    </div>
+                    <div className="cardText">
+                        <p>More than 2 billion people all over the world use socibooks to stay in touch with friends.</p>
+                        <div className="cardArrow">
+                            <p className="color">join our community</p>
+                            <img className="color" src="#" alt="arrow icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="awards">
+                    <div className="cardTitle">
+                        <img src="#" alt="trophy icon" />
+                        <h4>Awwwards</h4>
+                    </div>
+                    <div className="awardsContainer">
+                        <div className="awardsPhoto">
+                            <img src="#" alt="small profile of Allen" />
+                        </div>
+                        <div className="award">
+                            <p>Best of <span className="color">2021 </span>on Github</p>
+                        </div>
+                    </div>
+                    <div className="cardText">
+                        <p>More than 2 billion people all over the world use socibooks to stay in touch with friends.</p>
+                        <div className="cardArrow">
+                            <p>go to awards</p>
+                            <img src="#" alt="arrow icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="acheivement">
+                    <div className="aboutText">
+                        <h5>Our Acheivements</h5>
+                        <h3>we are connecting you to the digital life.</h3>
+                        <p className="aboutDescription"> The scope of Social Media becomes crucial to help businesses to directly engage with their needs and wants.</p>
+                        <div className="discoverButton">
+                            <button>Discover Me
+                                <img src="#" alt="arrow" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
-
 export default Main;
